95599f64971bcdd60b0e6dd5de26250a316c2640
Google Google <Google@students.softuni.bg>
2019-01-27 17:30:31 +0200

    Exercise DOM

--- 05.JSCore/06.ExerciseDOM/08_Toto-Numbers/solution.js
+++ 05.JSCore/06.ExerciseDOM/08_Toto-Numbers/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-	//TODO
+	let getNumbersBtn = document.getElementsByTagName("button")[0];
+
+	let inputField = document.getElementsByTagName("input")[0];
+
+	let targetDiv = document.getElementById("allNumbers");
+	
+	function CalculatingEndResult(inputNumbers, targetDiv){
+		for(i=1; i<=49; i++){
+			let div = document.createElement("div");
+			div.textContent = i.toString();
+			div.classList.add("numbers");
+
+			if(inputNumbers.includes(i.toString())){
+				div.style.backgroundColor = "orange";
+			}
+
+			targetDiv.appendChild(div);
+		}
+	}
+
+	getNumbersBtn.addEventListener("click", function(){
+		targetDiv.innerHTML = "";
+
+		let inputNumbers = inputField.value.split(" ");
+
+		if(inputNumbers.length == 6){
+
+			let invalidNumbersCount = 0;
+			
+			for(i=0; i<inputNumbers.length; i++){
+				if(inputNumbers[i] < 1 || inputNumbers[i] > 49){
+					invalidNumbersCount++;
+				}
+			}
+
+			if(invalidNumbersCount == 0){
+				CalculatingEndResult(inputNumbers, targetDiv);
+			}
+		}
+			
+		inputField.setAttribute("disabled", "");
+		getNumbersBtn.setAttribute("disabled","");
+	});
 }
--- 05.JSCore/06.ExerciseDOM/09_Virus-Installer/solution.js
+++ 05.JSCore/06.ExerciseDOM/09_Virus-Installer/solution.js
@@ -1,3 +1,70 @@
 function solve() {
-    //TODO
+    let nextBtn = document.getElementsByTagName("button")[0];
+    let cancelBtn = document.getElementsByTagName("button")[1];
+    cancelBtn.addEventListener("click", function(){
+        document.getElementsByTagName("section")[0].style.display = "none";
+    });
+
+    let firstStepDiv = document.getElementById("firstStep");
+    let secondStepDiv = document.getElementById("secondStep");
+    let thirdStepDiv = document.getElementById("thirdStep");
+
+    let licenseBtns = document.getElementsByName("license");
+
+    function FinalTimeOut() {
+        setTimeout(function(){
+
+        nextBtn.style.display = "inline";
+        
+        nextBtn.addEventListener("click", function(){
+            secondStepDiv.style.display = "none";
+            thirdStepDiv.style.display = "block";
+        })
+
+        }, 3000);
+    }
+
+    function ChangeBackground(){
+        document.getElementById("content").style.backgroundImage = "url()";
+        firstStepDiv.style.display = "block";
+    }
+
+    function DisplaySecondStep(){
+        Array.from(licenseBtns).forEach(function (currentBtn) {
+            if(currentBtn.hasAttribute("checked") && currentBtn.value == "agree"){
+                    firstStepDiv.style.display = "none";
+                    secondStepDiv.style.display = "block";
+                    nextBtn.style.display = "none";
+            }
+        })
+    }
+    Array.from(licenseBtns).forEach(function(currentBtn) {
+        currentBtn.addEventListener("click", function(){
+                
+            currentBtn.setAttribute("checked", "");
+
+                for(index=0; index<licenseBtns.length; index++){
+                    if(licenseBtns[index] != currentBtn){
+                        licenseBtns[index].removeAttribute("checked");
+                    }
+                }
+              
+        });   
+    })
+
+        nextBtn.addEventListener("click", function(){
+            
+            if(firstStepDiv.display != "block" && secondStepDiv.display != "block" && thirdStepDiv.display != "block"){
+                ChangeBackground();
+            }
+
+            if(firstStepDiv.display != "none" && secondStepDiv.display != "block" && thirdStepDiv.display != "block"){
+               DisplaySecondStep();
+            }
+
+            if(firstStepDiv.display != "block" && secondStepDiv.display != "none" && thirdStepDiv.display != "block"){
+                FinalTimeOut();
+            }
+
+        });
 }
