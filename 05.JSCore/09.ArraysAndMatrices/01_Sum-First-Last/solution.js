e0d5a5d3cda56e8926cf2f7e2cbf9a4f0c4737c1
Google Google <Google@students.softuni.bg>
2019-01-30 19:19:27 +0200

    ArraysAndMatricesLab

---
+++ 05.JSCore/09.ArraysAndMatrices/01_Sum-First-Last/solution.js
@@ -0,0 +1,13 @@
+function solve(numArr) {
+  let listInput = JSON.parse(document.getElementById("arr").value);
+  let divResult = document.getElementById('result');
+
+  function calculate(list) {
+    for (let i = 0; i < list.length; i++) {
+      let p = document.createElement('p');
+      p.textContent = `${i} -> ${list[i]*list.length}`;
+      divResult.appendChild(p);
+    }
+  }
+  calculate(listInput);
+}
