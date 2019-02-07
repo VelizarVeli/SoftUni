9fe38a110a7704897aa9e8fe949913c9d6ad11e8
Google Google <Google@students.softuni.bg>
2019-02-07 21:29:57 +0200

    StringsAndRegex

--- 05.JSCore/12.ExerciseStringsAndRegexp/02_Expedition-Problems/solution.js
+++ 05.JSCore/12.ExerciseStringsAndRegexp/02_Expedition-Problems/solution.js
@@ -1,3 +1,86 @@
 function solve() {
-  //TODO...
+  let inputStrElement = document.getElementById('str');
+  let inputTextElement = document.getElementById('text');
+  let outputElement = document.getElementById('result');
+
+  let pattern = /(north|east)(?:[\s\S])*?([\d]{2})(?:[^,]+)*?,(?:[^,])*?([\d]{6})/gmi;
+  let matches = inputTextElement.value.match(pattern);
+  let matchedInfo = [];
+
+  if (matches.length > 0) {
+    matches.forEach((match) => {
+      pattern = /(north|east)(?:[\s\S])*?([\d]{2})(?:[^,]+)*?,(?:[^,])*?([\d]{6})/gmi;
+      let result = pattern.exec(match);
+
+      if (result.length > 1) {
+        matchedInfo.push(result.slice(1, 4).join(" "));
+      }
+    });
+  } else {
+    return;
+  }
+
+  let northCoords = takeRightCoords('NORTH');
+  let eastCoords = takeRightCoords('EAST');
+
+  let message = inputTextElement.value.split(inputStrElement.value)[1];
+
+  appendToTheFather(`${northCoords[1]}.${northCoords[2]} ${northCoords[0][0]}`);
+  appendToTheFather(`${eastCoords[1]}.${eastCoords[2]} ${eastCoords[0][0]}`);
+  appendToTheFather(`Message: ${message}`);
+
+  function takeRightCoords(rightWay) {
+
+    console.log(matchedInfo);
+    return matchedInfo
+      .reverse()
+      .map((coords) => coords.toUpperCase())
+      .filter((coordsInfo) => coordsInfo
+        .includes(rightWay))[0].split(' ');
+  }
+
+  function appendToTheFather(text) {
+    let p = document.createElement('p');
+    p.textContent = text;
+    outputElement.appendChild(p);
+  }
+
+  inputStrElement.value = "";
+  inputTextElement.value = "";
 }
+//another solution
+// function solve() {
+//   let keyword = document.getElementById('str').value;
+//   let text = document.getElementById('text').value;
+
+//   let outputElement = document.getElementById('result');
+
+//   let regEx = new RegExp(`${keyword}(.*?)${keyword}`, "g");
+//   let message = `Message: ${regEx.exec(text)[1]}`;
+//   text = text.replace(message, "");
+
+//   let regex = /(east|north)[\s\S]*?([\d]{2})[^,]*?,[^,]*?([\d]{6})/gi;
+
+//   let m;
+
+//   let east = "";
+//   let north = "";
+
+//   while ((m = regex.exec(text)) !== null) {
+//     if (m[1].toUpperCase() === 'NORTH') {
+//       north = `${m[2]}.${m[3]} N`;
+//     } else if (m[1].toUpperCase() === "EAST") {
+//       east = `${m[2]}.${[m[3]]} E`;
+//     }
+//   }
+
+//   appendToParent(north);
+//   appendToParent(east);
+//   appendToParent(message);
+
+//   function appendToParent(text) {
+//     let p = document.createElement('p');
+//     p.textContent = text;
+//     outputElement.appendChild(p);
+//   }
+// }
--- 05.JSCore/12.ExerciseStringsAndRegexp/03_James-Bond/solution.js
+++ 05.JSCore/12.ExerciseStringsAndRegexp/03_James-Bond/solution.js
@@ -1,3 +1,51 @@
 function solve() {
-  //TODO...
+
+  let input = document.querySelector('#arr').value;
+  input = JSON.parse(input);
+  let output = document.querySelector('#result');
+  let key = input.shift();
+
+  let keyFinder = new RegExp(`((?<=(\\s)${key})|(?<=^${key}))\\s+([A-Z!%$#]{8,})([\\s.,]|$)`, 'gi');
+  let caseChecher = /[a-z]+/g;
+
+  let result;
+  let resultArr = [];
+
+  for (let i = 0; i < input.length; i++) {
+      resultArr[i] = input[i];
+      while (result = keyFinder.exec(input[i])) {
+          if (!caseChecher.exec(result[3])) {
+              input[i] = input[i].replace(result[3], decode(result[3]));
+              resultArr[i] = input[i];
+          }
+      }
+  }
+
+  for (let row of resultArr) {
+      createAddAppend('p', row, output);
+  }
+
+  function createAddAppend(child, text, parent) {
+      let childElement = document.createElement(child);
+      childElement.textContent = text;
+      parent.appendChild(childElement);
+  }
+  function decode(text) {
+      text = text.toLowerCase();
+      for (let char of text) {
+          if (text.includes('!')) {
+              text = text.replace('!', '1');
+          }
+          if (text.includes('%')) {
+              text = text.replace('%', '2');
+          }
+          if (text.includes('#')) {
+              text = text.replace('#', '3');
+          }
+          if (text.includes('$')) {
+              text = text.replace('$', '4');
+          }
+      }
+      return text;
+  }
 }
