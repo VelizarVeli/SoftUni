049e0774daaedf60154e93a25ae1ee151c132ff7
Google Google <Google@students.softuni.bg>
2019-05-21 22:06:04 +0300

    Created view for logged in user

---
+++ 04.C#Web/C# MVC Frameworks - ASP.NET Core/05.ExerciseIntroductionToASP.NetCore/Panda/Panda.Web/Panda.Web/Controllers/ReceiptController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Panda.Web.Controllers
+{
+    public class ReceiptController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView("Index");
+        }
+    }
+}
---
+++ 04.C#Web/C# MVC Frameworks - ASP.NET Core/05.ExerciseIntroductionToASP.NetCore/Panda/Panda.Web/Panda.Web/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Panda.Web.Controllers.Users
+//{
+//    public class UserController : Controller
+//    {
+//        public async Task<IActionResult> 
+//    }
+//}
